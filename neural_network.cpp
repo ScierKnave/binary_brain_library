2eec8016303d21dface73f6e0e168365680cef8e
Dereck <pichedereck@gmail.com>
2022-11-04 14:25:04 -0400

    initialise

---
+++ neural_network.cpp
@@ -0,0 +1,45 @@
+// function template
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+using namespace std;
+
+
+
+class neural_network{
+  public:
+    neural_network(){
+
+    }
+
+    void add_layer(int size, string a_fun){
+      //complete
+    }
+
+    void fit(int** X_train, int** Y_train, string loss_f){
+      //complete
+    }
+
+  private:
+    array<float, 0> W; //weight matrix
+    vector<float> G; //gradient
+
+    void gradient(){
+      //complete
+    }
+
+    void gradient_descent(){
+      //complete
+    }
+    
+
+};
+
+int main() {
+  neural_network gizzar;
+  cout << "works!";
+  return 0;
+}
+
+
