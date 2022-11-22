c5246c11346608630fdbc3e03951d2ec0b2536d4
Dereck <pichedereck@gmail.com>
2022-11-22 16:20:06 -0500

    renamed class

---
+++ fc_neural_network/src/ff_neural_network.cpp
@@ -0,0 +1,47 @@
+
+// function templates
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+using namespace std;
+
+//what is this 2 in the 4
+
+
+class ff_nn{
+    public:
+        ff_nn(){
+        //complete
+        }
+        void add_layer(int size, string a_fun){
+        //complete
+        }
+        void fit(int** X_train, int** Y_train, string loss_f){
+        //complete
+        }
+    private:
+
+        vector< vector<float> > W; //weight matrix
+        vector< vector<float> > G; //gradient
+        void forward_prop(){
+        //complete
+        }
+        void backward_prop(){
+            
+        //complete
+        }
+        void gradient_descent(){
+        //complete
+        }
+};
+
+int main() {
+    system("Pause");
+    return 0;
+}
+
+
+
+
+
